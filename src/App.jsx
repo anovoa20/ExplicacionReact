278227c3a48910bba83714c5bb1cce6da37b67b4
Andres Novoa <anovoa20@gmail.com>
2022-07-24 14:18:54 -0500

    Agregar page de bordercollie y dejar funcionando antes de poner el layout en app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "styles/styles.css";
 import Index from "pages";
 import RhodesianInfoPage from "pages/rhodesian";
+import BorderCollieInfoPage from "pages/bordercollie";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/rhodesian" element={<RhodesianInfoPage />} />
+          <Route path="/bordercollie" element={<BorderCollieInfoPage />} />
           <Route path="/" element={<Index />} />
         </Routes>
       </Router>
---
+++ src/pages/bordercollie.jsx
@@ -0,0 +1,11 @@
+import Layout from "layouts/Layout";
+
+function BorderCollieInfoPage() {
+  return (
+    <Layout>
+      <div>esta es la de Border Collie dentro de bordercollie jsx</div>
+    </Layout>
+  );
+}
+
+export default BorderCollieInfoPage;
